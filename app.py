43589f1418b5a345a18966d9708269f7c64643ce
prakharrai2512 <prakharrai2512@gmail.com>
2024-04-23 21:06:59 +0530

    sent

--- app.py
+++ app.py
@@ -5,19 +5,83 @@ from keras.models import load_model
 import streamlit as st
 import matplotlib.pyplot as plt
 import seaborn as sns
+import torch
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, Trainer, TrainingArguments
+import pandas as pd
+
+gpu_no = 0
+# Check GPU availability
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+
+# device
 
 sns.set(style='darkgrid')
 
+model_path_sent = './saved_model/'
+tokenizer = AutoTokenizer.from_pretrained(model_path_sent)
+model_sent = AutoModelForSequenceClassification.from_pretrained(model_path_sent)
+model_sent.to(device)
+
+import praw
+reddit = praw.Reddit(
+    client_id='MazNPVF4wTFD378C0wVJHg',
+    client_secret='02sXZyN4qv1z6n5wrlIP0gBDCVtGtQ',
+    user_agent='Sentiment analysis',
+)
+
+
+
+def get_model_op(text, tokenizer = tokenizer, model = model_sent):
+    label_text = ['postive', 'negative','neutral']
+    inputs = tokenizer(text, return_tensors='pt', truncation=True, padding=True).to(device)
+    # print(inputs)
+    with torch.no_grad():
+        logits = model(**inputs).logits
+
+    probabilities = torch.softmax(logits, dim=1)
+    # print(probabilities)
+    predicted_class = logits.argmax().item()
+    # predicted_label = params["index_to_label"].get(predicted_class, 'Unknown')
+    return label_text[predicted_class]#, round(probabilities[0][predicted_class].item(), 2),predicted_class,
+
+def display_reddit_posts_and_comments():
+    global stock
+    # stock = st.text_input('Enter Stock Symbol', 'AAPL').upper()
+
+    if stock:
+        st.header(f'Top Reddit posts and sentiment analysis for {stock}')
+        try:
+            subreddit = reddit.subreddit("all") 
+            posts = subreddit.search(stock, limit=5)
+
+            for post in posts:
+                with st.expander(f"{post.title}"):
+                    st.markdown(f"**Link:** [Here]({post.url})")
+                    st.markdown(f"**Post Content:** {post.selftext[:1000]}...")  
+                    sentiment = get_model_op(post.title + ' ' + post.selftext)
+                    st.markdown(f"**Post Sentiment:** {sentiment}")
+                    post.comments.replace_more(limit=0)
+                    comments = post.comments.list()[:5]  # Top 5 comments
+                    for comment in comments:
+                        st.markdown(f"**Comment:** {comment.body[:1000]}...") 
+                        comment_sentiment = get_model_op(comment.body)
+                        st.markdown(f"**Comment Sentiment:** {comment_sentiment}")
+        except Exception as e:
+            st.error("Failed to fetch Reddit data. Check your credentials and network connection.")
+
+
 plt.style.use('dark_background')
 
-model = load_model('/Users/ramdabas/Downloads/Stock_Market_Prediction_ML 2/Stock Predictions Model.keras')
-
-st.header('Stock Market Predictor')
+model = load_model('./Stock Predictions Model.keras')
+st.title('Multi-Modal Stock Information Retrival and Prediction')
 
 stock =st.text_input('Enter Stock Symbol', 'GOOG')
 start = '2012-01-01'
 end = '2022-12-31'
-
+display_reddit_posts_and_comments()
 data = yf.download(stock, start ,end)
 
 st.subheader('Stock Data')
